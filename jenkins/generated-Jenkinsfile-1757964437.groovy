0e99e661712760d614e87b9c82822855b23a0a7f
Lavanya5577 <145743443+Lavanya5577@users.noreply.github.com>
2025-09-16 01:30:57 +0530

    Update generated-Jenkinsfile-1757964437.groovy

--- jenkins/generated-Jenkinsfile-1757964437.groovy
+++ jenkins/generated-Jenkinsfile-1757964437.groovy
@@ -1,9 +1,14 @@
 pipeline {
     agent any
     tools {
-        nodejs 'Node16'  // Name you configured in Jenkins
+        nodejs 'Node16'  // must match exactly the name in Jenkins Tools
     }
     stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Lavanya5577/node_pro.git'
+            }
+        }
         stage('Install Dependencies') {
             steps {
                 sh 'npm install'
